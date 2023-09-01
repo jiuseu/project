f61c781c203d1a48f724274755c7745f99b15010
lee hyun woo <kaway0418@naver.com>
2023-09-01 22:41:03 +0900

    2023/9/01 Board, repository 추가

---
+++ src/test/java/com/example/project/repository/repositoryTests.java
@@ -0,0 +1,81 @@
+package com.example.project.repository;
+
+import com.example.project.domain.Board;
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.IntStream;
+
+@SpringBootTest
+@Log4j2
+public class repositoryTests {
+
+    @Autowired
+    private BoardRepository boardRepository;
+
+    @Test
+    public void InsertTest(){
+
+        IntStream.rangeClosed(1,100).forEach(i -> {
+            Board board = Board.builder()
+                    .title("title.."+i)
+                    .content("content.."+i)
+                    .writer("user"+(i%10))
+                    .build();
+
+            Board result = boardRepository.save(board);
+            log.info("BNO: "+result.getBno());
+        });
+    }
+
+    @Test
+    public void SelectTest(){
+        long num = 10L;
+
+        Optional<Board> result = boardRepository.findById(num);
+        Board board = result.orElseThrow();
+
+        log.info(board);
+    }
+
+    @Test
+    public void UpdateTest(){
+        long num = 100L;
+
+        Optional<Board> result = boardRepository.findById(num);
+        Board board = result.orElseThrow();
+        board.change("update..title 100", "update..content 100");
+
+        boardRepository.save(board);
+        log.info(board);
+    }
+
+    @Test
+    public void DeleteTest(){
+        long num = 1L;
+        boardRepository.deleteById(num);
+    }
+
+    @Test
+    public void PagingTest(){
+        Pageable pageable = PageRequest.of(0,10, Sort.by("bno").descending());
+
+        Page<Board> result = boardRepository.findAll(pageable);
+
+        log.info("Total count: "+result.getTotalElements());
+        log.info("Total pages: "+result.getTotalPages());
+        log.info("Page number: "+result.getNumber());
+        log.info("Page size: "+result.getSize());
+
+        List<Board> list = result.getContent();
+        list.forEach(board -> log.info(board));
+    }
+}
