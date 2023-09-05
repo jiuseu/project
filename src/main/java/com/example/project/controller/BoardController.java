1aefc47f6facd77320030646e6b28a3d7b23eb6c
lee hyun woo <kaway0418@naver.com>
2023-09-05 11:35:17 +0900

    2023/9/05 목록화면 추가(컨트롤러,html)

--- src/main/java/com/example/project/controller/BoardController.java
+++ src/main/java/com/example/project/controller/BoardController.java
@@ -1,8 +1,12 @@
 package com.example.project.controller;
 
+import com.example.project.dto.BoardDTO;
 import com.example.project.dto.PageRequestDTO;
+import com.example.project.dto.PageResponseDTO;
+import com.example.project.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,8 +18,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 public class BoardController {
 
+    private final BoardService boardService;
+
     @GetMapping("/list")
     public void list(PageRequestDTO pageRequestDTO, Model model){
 
+        PageResponseDTO<BoardDTO> responseDTO = boardService.list(pageRequestDTO);
+
+        log.info(responseDTO);
+
+        model.addAttribute("responseDTO",responseDTO);
     }
 }
