09996b8cdc47d9c4754fa074dc45e8b8be170c39
lee hyun woo <kaway0418@naver.com>
2023-09-09 17:28:24 +0900

    9/9 swagger-ui 추가

--- src/main/java/com/example/project/controller/BoardController.java
+++ src/main/java/com/example/project/controller/BoardController.java
@@ -14,9 +14,10 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-@Controller
+@RestController
 @Log4j2
 @RequestMapping("/board")
 @RequiredArgsConstructor
