90133c5ef3d70660f32fe8bd84bab1129222c3aa
lee hyun woo <kaway0418@naver.com>
2023-09-12 22:47:25 +0900

    9/12 400 bindResult 기능 추가

--- src/main/java/com/example/project/controller/ReplyController.java
+++ src/main/java/com/example/project/controller/ReplyController.java
@@ -2,15 +2,19 @@ package com.example.project.controller;
 
 import com.example.project.dto.ReplyDTO;
 import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.awt.*;
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
@@ -20,11 +24,18 @@ public class ReplyController {
 
     @Operation(summary = "Replies POST",description = "POST 방식으로 댓글 등록")
     @PostMapping(value = "/", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Map<String,Long>> register(@RequestBody ReplyDTO replyDTO){
+    public Map<String,Long> register(@Valid @RequestBody ReplyDTO replyDTO,
+                                                     BindingResult bindingResult)throws BindException{
 
         log.info(replyDTO);
-        Map<String, Long> resultMap = Map.of("rno",111L);
 
-        return ResponseEntity.ok(resultMap);
+        if(bindingResult.hasErrors()){
+            throw new BindException(bindingResult);
+        }
+
+        Map<String, Long> resultMap = new HashMap<>();
+        resultMap.put("rno",111L);
+
+        return resultMap;
     }
 }
