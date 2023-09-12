d662e0af5ceab6eb7594b52bceddf5b494df2ef8
lee hyun woo <kaway0418@naver.com>
2023-09-12 22:47:54 +0900

    9/12 Valid 어노테이션 추가

--- src/main/java/com/example/project/dto/ReplyDTO.java
+++ src/main/java/com/example/project/dto/ReplyDTO.java
@@ -1,5 +1,7 @@
 package com.example.project.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,10 +17,13 @@ public class ReplyDTO {
 
     private Long rno;
 
+    @NotNull
     private Long bno;
 
+    @NotEmpty
     private String replyText;
 
+    @NotEmpty
     private String replyer;
 
     private LocalDateTime regDate, modDate;
