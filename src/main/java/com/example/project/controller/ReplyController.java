08ad8f9d261094f2390d0dc55257b50b6c17001a
lee hyun woo <kaway0418@naver.com>
2023-09-13 17:32:19 +0900

    9/13 댓글 crud 컨트롤러 추가

--- src/main/java/com/example/project/controller/ReplyController.java
+++ src/main/java/com/example/project/controller/ReplyController.java
@@ -1,17 +1,18 @@
 package com.example.project.controller;
 
+import com.example.project.dto.PageRequestDTO;
+import com.example.project.dto.PageResponseDTO;
 import com.example.project.dto.ReplyDTO;
+import com.example.project.service.ReplyService;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.awt.*;
 import java.util.HashMap;
@@ -20,7 +21,10 @@ import java.util.Map;
 @RestController
 @RequestMapping("/replies")
 @Log4j2
+@RequiredArgsConstructor
 public class ReplyController {
+
+    private final ReplyService replyService;
 
     @Operation(summary = "Replies POST",description = "POST 방식으로 댓글 등록")
     @PostMapping(value = "/", consumes = MediaType.APPLICATION_JSON_VALUE)
@@ -34,7 +38,52 @@ public class ReplyController {
         }
 
         Map<String, Long> resultMap = new HashMap<>();
-        resultMap.put("rno",111L);
+        Long rno = replyService.register(replyDTO);
+        resultMap.put("rno", rno);
+
+        return resultMap;
+    }
+
+    @Operation(summary = "Replies Of Board",description = "GET 방식으로 특정 게시물의 댓글 목록")
+    @GetMapping(value = "/list/{bno}")
+    public PageResponseDTO<ReplyDTO> getList(@PathVariable("bno") Long bno,
+                                             PageRequestDTO pageRequestDTO){
+
+        PageResponseDTO<ReplyDTO> responseDTO = replyService.getListBoard(bno,pageRequestDTO);
+
+        return responseDTO;
+    }
+
+    @Operation(summary = "Read Reply", description = "GET 방식으로 특정 댓글 조회")
+    @GetMapping("/{rno}")
+    public ReplyDTO getReplyDTO(@PathVariable("rno") Long rno){
+
+        ReplyDTO replyDTO = replyService.read(rno);
+
+        return replyDTO;
+    }
+
+    @Operation(summary =  "DELETE 방식으로 특정 댓글 삭제")
+    @DeleteMapping("/{rno}")
+    public Map<String,Long> remove( @PathVariable("rno") Long rno ){
+
+        replyService.remove(rno);
+
+        Map<String, Long> resultMap = new HashMap<>();
+
+        resultMap.put("rno", rno);
+
+        return resultMap;
+    }
+
+    @Operation(summary = "PUT 방식으로 특정 댓글 수정")
+    @PutMapping("/{rno}")
+    public Map<String, Long> modify(@PathVariable("rno") Long rno, @RequestBody ReplyDTO replyDTO){
+
+        replyDTO.setRno(rno);
+        replyService.modify(replyDTO);
+        Map<String, Long> resultMap = new HashMap<>();
+        resultMap.put("rno" , rno);
 
         return resultMap;
     }
