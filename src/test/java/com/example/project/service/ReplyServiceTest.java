8945631496f03495dedb89b50b1f55a43fbbb8aa
lee hyun woo <kaway0418@naver.com>
2023-09-13 17:34:17 +0900

    9/13 ReplyService 테스트 케이스

---
+++ src/test/java/com/example/project/service/ReplyServiceTest.java
@@ -0,0 +1,28 @@
+package com.example.project.service;
+
+import com.example.project.dto.ReplyDTO;
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Log4j2
+public class ReplyServiceTest {
+
+    @Autowired
+    private ReplyService replyService;
+
+    @Test
+    public void registerTest(){
+
+        ReplyDTO replyDTO = ReplyDTO.builder()
+                .replyText("댓글 테스트333~")
+                .replyer("댓글이")
+                .bno(103L)
+                .build();
+
+        Long bno = replyService.register(replyDTO);
+        log.info(bno);
+    }
+}
