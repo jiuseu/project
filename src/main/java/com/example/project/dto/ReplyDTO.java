3def7ad4a4f0f8463b8c2a1ca2d4b65efc7986f6
lee hyun woo <kaway0418@naver.com>
2023-09-21 22:12:33 +0900

    9/21 JsonFormat, JsonIgnore 어노테이션 추가

--- src/main/java/com/example/project/dto/ReplyDTO.java
+++ src/main/java/com/example/project/dto/ReplyDTO.java
@@ -1,5 +1,7 @@
 package com.example.project.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -26,5 +28,9 @@ public class ReplyDTO {
     @NotEmpty
     private String replyer;
 
-    private LocalDateTime regDate, modDate;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime regDate;
+
+    @JsonIgnore
+    private LocalDateTime modDate;
 }
