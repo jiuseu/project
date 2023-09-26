fb09addda6c080b6e5303a9459ad0fb95ce61542
lee hyun woo <kaway0418@naver.com>
2023-09-26 20:34:52 +0900

    9/26 업로드 처리 DTO 생성

---
+++ src/main/java/com/example/project/dto/UploadFileDTO.java
@@ -0,0 +1,12 @@
+package com.example.project.dto;
+
+import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@Data
+public class UploadFileDTO {
+
+    private List<MultipartFile> files;
+}
