8347919d7c760a492653ce18d333e12fb636cfd2
lee hyun woo <kaway0418@naver.com>
2023-09-27 20:39:48 +0900

    9/27 쿼리 메소드 추가

--- src/main/java/com/example/project/repository/BoardRepository.java
+++ src/main/java/com/example/project/repository/BoardRepository.java
@@ -2,11 +2,15 @@ package com.example.project.repository;
 
 import com.example.project.domain.Board;
 import com.example.project.repository.search.BoardSearch;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.Optional;
+
 public interface BoardRepository extends JpaRepository<Board, Long>, BoardSearch {
 
-    @Query(value = "select now()", nativeQuery = true)
-    String getTime();
+    @EntityGraph(attributePaths = {"imageSet"})
+    @Query(value = "select b from Board b where b.bno =:bno")
+    Optional<Board> findByIdWithImages(Long bno);
 }
