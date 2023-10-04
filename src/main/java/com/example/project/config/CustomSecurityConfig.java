25c4d835ada8052fb672f829bb17b7cb447ea4bd
lee hyun woo <kaway0418@naver.com>
2023-10-04 18:24:35 +0900

    10/4 webSecurity 커스텀 메소드 설정 추가

--- src/main/java/com/example/project/config/CustomSecurityConfig.java
+++ src/main/java/com/example/project/config/CustomSecurityConfig.java
@@ -2,9 +2,11 @@ package com.example.project.config;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Log4j2
@@ -17,5 +19,14 @@ public class CustomSecurityConfig {
         log.info("-----------------------configure-----------------------");
 
         return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer(){
+
+        log.info("-----------------------web configure-----------------------");
+
+        return (web) -> web.ignoring().requestMatchers(PathRequest
+                .toStaticResources().atCommonLocations());
     }
 }
