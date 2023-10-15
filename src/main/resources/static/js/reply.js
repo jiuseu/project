25e12f0249a0e3488f6270c5d1d32df052d04b52
lee hyun woo <kaway0418@naver.com>
2023-10-15 21:38:19 +0900

    10/15

--- src/main/resources/static/js/reply.js
+++ src/main/resources/static/js/reply.js
@@ -4,7 +4,6 @@ async function get1(bno){
   return result
 }
 
-
 async function getList({bno, page, size, goLast}){
 
    const result = await axios.get(`/replies/list/${bno}`, {params: {page, size}})
