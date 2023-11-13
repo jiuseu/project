50a237add4450bc375b0ba275279192229bc74ca
lee hyun woo <kaway0418@naver.com>
2023-11-13 21:35:00 +0900

    11/13

--- src/main/java/com/example/project/config/CustomSecurityConfig.java
+++ src/main/java/com/example/project/config/CustomSecurityConfig.java
@@ -5,6 +5,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
@@ -12,6 +13,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @Log4j2
 @Configuration
 @RequiredArgsConstructor
+@EnableMethodSecurity
 public class CustomSecurityConfig {
 
     @Bean
