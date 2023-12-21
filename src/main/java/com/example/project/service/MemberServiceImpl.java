f7c980b0e98f152404f84138911df0b4fbcc612e
lee hyun woo <kaway0418@naver.com>
2023-12-21 21:05:25 +0900

    12/21

--- src/main/java/com/example/project/service/MemberServiceImpl.java
+++ src/main/java/com/example/project/service/MemberServiceImpl.java
@@ -1,8 +1,11 @@
 package com.example.project.service;
 
 import com.example.project.dto.MemberJoinDTO;
+import com.example.project.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.modelmapper.ModelMapper;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,8 +13,13 @@ import org.springframework.stereotype.Service;
 @Log4j2
 public class MemberServiceImpl implements MemberService{
 
+    private final ModelMapper modelMapper;
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
     @Override
     public void join(MemberJoinDTO memberJoinDTO)throws MidExistException{
+
+        log.info("Member Join...");
 
     }
 }
