6e6a68bd97173699b76284f13dd69dda53beef7c
lee hyun woo <kaway0418@naver.com>
2023-12-29 18:40:42 +0900

    12/29

--- src/main/java/com/example/project/repository/MemberRepository.java
+++ src/main/java/com/example/project/repository/MemberRepository.java
@@ -14,4 +14,5 @@ public interface MemberRepository extends JpaRepository<Member, String> {
     @Query("select m from Member m where m.mid =:mid and m.social = false")
     Optional<Member> getWithRoles(@Param("mid") String mid);
 
+
 }
