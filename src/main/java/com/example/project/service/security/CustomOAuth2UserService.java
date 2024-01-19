f117f9a76432fa14688551d04191a9e3cad51945
lee hyun woo <kaway0418@naver.com>
2024-01-19 19:27:27 +0900

    1/19

--- src/main/java/com/example/project/service/security/CustomOAuth2UserService.java
+++ src/main/java/com/example/project/service/security/CustomOAuth2UserService.java
@@ -2,11 +2,14 @@ package com.example.project.service.security;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @Log4j2
 @Service
@@ -21,7 +24,19 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 
         log.info("oauth2 user.....................................");
 
+        ClientRegistration clientRegistration = userRequest.getClientRegistration();
+        String clientName = clientRegistration.getClientName();
 
-        return super.loadUser(userRequest);
+        log.info("NAME: "+clientName);
+
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+        Map<String, Object> paramMap = oAuth2User.getAttributes();
+
+        paramMap.forEach((k, v) -> {
+            log.info("--------------------------------------");
+            log.info(k +":" + v);
+        });
+
+        return oAuth2User;
     }
 }
