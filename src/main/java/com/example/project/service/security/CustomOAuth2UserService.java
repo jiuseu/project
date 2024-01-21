ff42a2ca07e4662b2adf704c1a20dce52a36cd3c
lee hyun woo <kaway0418@naver.com>
2024-01-21 21:18:02 +0900

    1/21

--- src/main/java/com/example/project/service/security/CustomOAuth2UserService.java
+++ src/main/java/com/example/project/service/security/CustomOAuth2UserService.java
@@ -54,6 +54,8 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 
     private String getKakaoEmail(Map<String, Object> paramMap){
 
+        log.info("KAKAO-------------------------------------------");
+
         return null;
     }
 }
