388131c256b9aaabf23b5a6cfdfdeb40926dfa54
lee hyun woo <kaway0418@naver.com>
2024-01-31 20:45:35 +0900

    1/31

--- src/main/java/com/example/project/service/security/CustomOAuth2UserService.java
+++ src/main/java/com/example/project/service/security/CustomOAuth2UserService.java
@@ -1,9 +1,11 @@
 package com.example.project.service.security;
 
 import com.example.project.domain.Member;
+import com.example.project.domain.MemberRole;
 import com.example.project.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
@@ -12,9 +14,11 @@ import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Log4j2
 @Service
@@ -65,12 +69,36 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
         Optional<Member> result = memberRepository.findByEmail(email);
 
         if(result.isEmpty()){
+            Member member = Member.builder()
+                    .mid(email)
+                    .mpw(passwordEncoder.encode("1111"))
+                    .email(email)
+                    .social(true)
+                    .build();
+            member.addRole(MemberRole.USER);
+            memberRepository.save(member);
 
+            MemberSecurityDTO memberSecurityDTO = new MemberSecurityDTO(
+                    email,"1111",email,false, true, Arrays.asList(
+                            new SimpleGrantedAuthority("ROLE_USER")
+            ));
+        return memberSecurityDTO;
         }else{
+            Member member = result.get();
+            MemberSecurityDTO memberSecurityDTO = new MemberSecurityDTO(
+              member.getMid(),
+              member.getMpw(),
+              member.getEmail(),
+              member.isDel(),
+              member.isSocial(),
+              member.getRoleSet().stream().map(memberRole -> new SimpleGrantedAuthority(
+                "ROLE_"+memberRole.name()
+              )).collect(Collectors.toList()));
 
+            return memberSecurityDTO;
         }
 
-        return null;
+
     }
 
     private String getKakaoEmail(Map<String, Object> paramMap){
