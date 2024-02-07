a5985055b5b42513f2c6f2d09ede9e804110751d
lee hyun woo <kaway0418@naver.com>
2024-02-07 20:52:24 +0900

    2/7

--- src/main/java/com/example/project/config/CustomSecurityConfig.java
+++ src/main/java/com/example/project/config/CustomSecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.project.config;
 
 import com.example.project.service.security.handler.Custom403Handler;
 import com.example.project.service.security.CustomUserDetailsService;
+import com.example.project.service.security.handler.CustomSocialLoginSuccessHandler;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
@@ -14,6 +15,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
 import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 
@@ -73,5 +75,8 @@ public class CustomSecurityConfig {
         return new Custom403Handler();
     }
 
-
+    @Bean
+    public AuthenticationSuccessHandler authenticationSuccessHandler(){
+        return new CustomSocialLoginSuccessHandler(passwordEncoder());
+    }
 }
