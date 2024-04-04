175fe300d2ee5d845745d2dcc393f55507f4b5c4
lee hyun woo <kaway0418@naver.com>
2024-04-04 20:09:56 +0900

    4/4

--- src/main/java/com/example/project/service/BoardServiceImpl.java
+++ src/main/java/com/example/project/service/BoardServiceImpl.java
@@ -95,6 +95,7 @@ public class BoardServiceImpl implements BoardService{
     @Override
     public PageResponseDTO<BoardListReplyCountDTO> listWithReplyCount(PageRequestDTO pageRequestDTO){
 
+        log.info("=================== Board Service List Reply Counting... ===================");
         String[] types = pageRequestDTO.getTypes();
         String keyword = pageRequestDTO.getKeyword();
         Pageable pageable = pageRequestDTO.getPageable("bno");
