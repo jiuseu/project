2142e1a115b438b6f935324d21f5788be56bbc2a
lee hyun woo <kaway0418@naver.com>
2024-05-09 20:03:02 +0900

    5/9

--- src/main/java/com/example/project/service/ReplyServiceImpl.java
+++ src/main/java/com/example/project/service/ReplyServiceImpl.java
@@ -29,7 +29,7 @@ public class ReplyServiceImpl implements ReplyService{
     @Override
     public Long register(ReplyDTO replyDTO){
 
-        log.info("=================== Reply Service register"+replyDTO.getRno()+"... ===================");
+        log.info("=================== Reply Service Register"+replyDTO.getRno()+"... ===================");
         modelMapper.typeMap(ReplyDTO.class, Reply.class).addMapping(
                 src -> src.getBno(), (dest, v) -> dest.getBoard().setBno((Long) v)
         );
