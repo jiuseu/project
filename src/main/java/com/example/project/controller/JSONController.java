669b25b0764e065fd97b7f3bc660462830cb797f
lee hyun woo <kaway0418@naver.com>
2024-07-10 20:08:50 +0900

    7/10

--- src/main/java/com/example/project/controller/JSONController.java
+++ src/main/java/com/example/project/controller/JSONController.java
@@ -12,6 +12,6 @@ public class JSONController {
     public String[] helloArr(){
 
         log.info("Hello Arr.......................");
-        return new String[]{"AAA","BBB","CCC","DDD"};
+        return new String[]{"AAA","BBB","CCC","DDD","FX-570CW"};
     }
 }
