dd7684b88713bf7e315dff9571097df6947ebf12
lee hyun woo <kaway0418@naver.com>
2025-02-27 11:42:23 +0900

    2/27

--- src/test/java/com/example/project/repository/BoardRepositoryTests.java
+++ src/test/java/com/example/project/repository/BoardRepositoryTests.java
@@ -5,13 +5,16 @@ import com.example.project.domain.BoardImage;
 import com.example.project.dto.BoardListAllDTO;
 import com.example.project.dto.BoardListReplyCountDTO;
 import lombok.extern.log4j.Log4j2;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+
 import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 
